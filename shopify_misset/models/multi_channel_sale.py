79c55eec48442b63529bd33e151f5a2c6095ce93
root <root@rai-odoo-misset-testaccept.novalocal>
2023-11-29 11:14:38 +0000

    App: Shopify misset
     Committer: deepa4lok deepa4lok@yahoo.com>

---
+++ shopify_misset/models/multi_channel_sale.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+
+from odoo import api,fields,models,exceptions
+
+from logging import getLogger
+_logger = getLogger(__name__)
+
+
+
+
+#class MultiChannelSale(models.Model):
+#	_inherit = 'multi.channel.sale'
+
+
