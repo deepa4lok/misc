ef6836ce911aac8cb6a586a1a0af4c6c32e048d2
root <root@rai-odoo-misset-testaccept.novalocal>
2023-11-29 11:16:35 +0000

    App: Shopify misset
     Committer: deepa4lok <deepa4lok@yahoo.com>

---
+++ shopify_misset/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+
+{
+    "name": "Shopify for Misset",
+    "summary": "Shopify improvements for Misset",
+    "version": "10.0.0.0",
+    'category': 'Tools',
+    'website' : "https://www.tosc.nl/",
+    "author": "Deepa, " "The Open Source company (TOSC)",
+    "license": "LGPL-3",
+    "depends": [
+        "shopify_odoo_bridge",
+        "odoo_multi_channel_sale",
+    ],
+    "data": [
+        "views/multi_channel_sale.xml",
+    ],
+}
+
