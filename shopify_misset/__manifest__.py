28176b793770166406752dd09be58d0d726ce97a
Deepa Venkatesh <deepa4lok@yahoo.com>
2023-12-01 15:34:18 +0530

    Shopify Misset: 10.0.1.0

--- shopify_misset/__manifest__.py
+++ shopify_misset/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Shopify for Misset",
     "summary": "Shopify improvements for Misset",
-    "version": "10.0.0.0",
+    "version": "10.0.1.0",
     'category': 'Tools',
     'website' : "https://www.tosc.nl/",
     "author": "Deepa, " "The Open Source company (TOSC)",
