f7a6cb9d58ac132c9b2f26344a50bee9bfac48af
Deepa Venkatesh <deepa4lok@yahoo.com>
2024-09-12 11:39:45 +0530

    App: rest_api_extended

---
+++ rest_api_extended/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2024 The Open Source Company (<https://www.tosc.nl>)
+
+{
+    "name": "Rest API - Extended",
+    "version": "16.0.0.0",
+    "author": "Deepa Venkatesh (DK), The Open Source Company (TOSC)",
+    "license": "AGPL-3",
+    "website": "www.tosc.nl",
+    "summary": "This app supports accepting of Analytic Ref & Country Code",
+    "description": '''
+Description
+-----------
+This app accepts Analytic Account Code/Ref sent as 'analytic_account_code', & then finds & updates with Analytic Account 'analytic_account_id'.
+similarly for Country Code sent as 'country_code', will be replaced with 'country_id'
+''',
+    "category": "Other",
+    "depends": [
+        'rest_api',
+    ],
+    "data": [],
+    "qweb":[],
+    "auto_install": False,
+    "installable": True,
+    "application": False,
+    "external_dependencies": {
+        'python': [],
+    },
+}
