0b91ce8a62a644c4a0cef9be8f05271a9d0f06f8
Deepa Venkatesh <deepa4lok@yahoo.com>
2024-12-09 14:17:02 +0530

    [MIG][392] supplier_bank_change_approval & other

--- supplier_bank_change_approval/__manifest__.py
+++ supplier_bank_change_approval/__manifest__.py
@@ -1,9 +1,9 @@
 # -*- encoding: utf-8 -*-
+
 ##############################################################################
 #
 #    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2022 Magnus (<http://www.magnus.nl>). All Rights Reserved
-#    Copyright (C) 2022-2024 TOSC (<http://www.tosc.nl>). All Rights Reserved
+#    Copyright (C) 2022-present TOSC (<http://www.tosc.nl>). All Rights Reserved
 #    $Id$
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -20,33 +20,39 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+
 {
-    'name': "Invoice Payment Order Check",
+    'name': "Supplier Bank Account Approval",
 
     'summary': """
-        Payment Order Preview on Invoice""",
+        Supplier Bank Account Approval""",
 
     'description': """
-        Module that allows the users to check the invoices that are part of a payment order.
+        \n1.Add state draft and confirmed to the banks_account_ids from partner.
+        \nThis should only apply to partners is supplier = true
+        \n2.New menu called 'Sensitive fields approval' where any changes can be approved. 
+        \nThis menu should only be visible and available to new security group 'Bank Account Manager'
+        \n3.When a bank account is changed or added, it will be in draft state. It cannot be used in any invoice or payment.
+        \nwhen creating an invoice or payment which says that "The supplier has changed bank details which are not yet approved.
+        """,
 
-    """,
-
-    'author': "K.Sushma, Deepa Venkatesh (DK), The Open Source Company (TOSC)",
+    'author': "TOSC-Sify, Deepa Venkatesh (DK), The Open Source Company (TOSC)",
     'website': "http://www.tosc.nl",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/10.0/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Accounting',
-    'version': '16.0.0.2',
+    'category': 'Uncategorized',
+    'version': '16.0.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['account', 'account_payment_order'],
+    'depends': ['base','account','account_payment_order', 'partner_manual_rank'],
 
     # always loaded
     'data': [
-        'security/payment_security.xml',
-        'views/invoice_views.xml',
+        'security/security.xml',
+        'views/views.xml',
+
     ],
     # only loaded in demonstration mode
     'demo': [],
